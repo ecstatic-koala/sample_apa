07417f7a2e7e655869b192dd05279b772c164751
Ecstatic Koala <spamify3@gmail.com>
2013-03-06 16:11:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApa::Application.config.secret_token = 'e8895d024d44367390c808ecf4d8c9b515ff634b51b070f97cb415729f48a84c4ac34b1bfc484177d828d8fe155801c10c03c6ed3de398e77136043684629488'
