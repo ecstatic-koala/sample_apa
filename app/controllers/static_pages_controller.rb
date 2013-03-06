84a68f42060035132593a4380d9f80c2eaf3f2a6
Ecstatic Koala <spamify3@gmail.com>
2013-03-06 17:35:27 +0100

    Added static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 # StaticPagesController inherits from the Rails class ApplicationController
 class StaticPagesController < ApplicationController
   def home
-    puts "home"
+    puts "home <--------------------- // StaticPagesController"
   end
 
   def help
-    puts "help"  
+    puts "help <--------------------- // StaticPagesController"
   end
+  
+  def about
+    puts "about <--------------------- // StaticPagesController"
+  end  
 end
